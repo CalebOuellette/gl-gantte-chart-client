5dd41c6cb31ae2293b7e5408bfc68c26d1477b9d
CalebOuellette <caleb.ouellette@gmail.com>
2017-05-14 00:06:37 -0700

    Add time adjust. Add task expand.

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, HostListener } from '@angular/core';
-import  { Task } from '../classes/task';
+import { Task } from '../classes/task';
+
 
 @Component({
   selector: 'gl-chart',
@@ -14,21 +15,24 @@ export class ChartComponent implements OnInit {
 
 
   public channels: Array<Task> = [
-    {id: 'A', color: "#F2C94C", startDate: new Date(), totalTime: 345600000},    
-    {id: 'B', color: "#F2994A", startDate: new Date(), totalTime: 345600000},    
-    {id: 'C', color: "#EB5757", startDate: new Date(), totalTime: 345600000},    
-    ];
+    { id: 'A', color: "#F2C94C", startDate: new Date(), totalTime: 345600000 },
+    { id: 'B', color: "#F2994A", startDate: new Date(), totalTime: 345600000 },
+    { id: 'C', color: "#EB5757", startDate: new Date(), totalTime: 345600000 },
+  ];
 
   private dragging: boolean = false;
   private dragItem: Task;
 
   private last: MouseEvent;
 
-  private offset: number = 400;  
-  
+  private offset: number = 400;
+
   public nowOffset: number = 125;
 
   public zoomScale: number = 1000000;
+
+  public dragEventType: number = 0;
+
 
   constructor() {
     this.channels[0].startDate.setHours(this.channels[0].startDate.getHours() + 10);
@@ -42,33 +46,50 @@ export class ChartComponent implements OnInit {
 
 
   @HostListener('mouseup')
-  onMouseup(event: MouseEvent) {    
-    this.dragging = false;      
-    this.dragItem = null;  
+  onMouseup(event: MouseEvent) {
+    this.dragging = false;
+    this.dragItem = null;
+    this.dragEventType = 0;
   }
 
   @HostListener('mousemove', ['$event'])
-  onMousemove(event: MouseEvent) {    
-    if (this.dragging) {
-      this.dragItem.totalTime = this.dragItem.totalTime + ((event.clientX - this.last.clientX) * this.zoomScale) ;
-      this.last = event;
+  onMousemove(event: MouseEvent) {
+    if (this.dragging && this.dragEventType) {
+      if (this.dragEventType == 1) {
+        this.dragItem.totalTime = this.dragItem.totalTime + ((event.clientX - this.last.clientX) * this.zoomScale);
+        this.last = event;
+      }
+      else if (this.dragEventType == 2) {
+        this.dragItem.startDate =new Date(this.dragItem.startDate.getTime() - ((event.clientX - this.last.clientX) * this.zoomScale));
+        this.last = event;
+      }
     }
   }
 
+
+
   @HostListener('mousedown', ['$event'])
-  onMousedown(event: MouseEvent) {  
-   
+  onMousedown(event: MouseEvent) {
+
   }
 
-  private taskSelect(task: Task, event: MouseEvent) {    
+  public taskExpand(task: Task, event: MouseEvent) {
     this.dragItem = task;
     this.dragging = true;
     this.last = event;
+    this.dragEventType = 1;
   }
 
 
-  public addTask(channel: number){
-    
+  public taskMove(task: Task, event: MouseEvent) {
+    this.dragItem = task;
+    this.dragging = true;
+    this.last = event;
+    this.dragEventType = 2;
+  }
+
+  public addTask(channel: number) {
+
   }
 
 }
