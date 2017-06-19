9dcc7c3fb40a9a70e35eb5015c6ad3d4fdbee4d1
CalebOuellette <caleb.ouellette@gmail.com>
2017-06-19 08:21:47 -0700

    Moved Project service up to app level.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
+import { ProjectService } from './services/project.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ProjectService]
 })
 export class AppComponent implements OnInit {
   ngOnInit() {
